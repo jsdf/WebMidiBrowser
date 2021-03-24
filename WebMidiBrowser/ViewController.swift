42e6083ab999f5784866e64d803e79f2869fcde5
James Friend <james@kitefire.com>
2021-03-23 21:25:28 -0400

    Initial Commit

---
+++ WebMidiBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebMidiBrowser
+//
+//  Created by James Friend on 3/23/21.
+//  Copyright © 2021 jsdf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
